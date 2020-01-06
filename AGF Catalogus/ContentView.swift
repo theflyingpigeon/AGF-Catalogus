8e943a323c4c4c8b0c65624f390dd4d3b7c0789d
shaeme <theflyingpigeon@zohomail.eu>
2020-01-06 19:41:16 +0100

    Initial Commit

---
+++ AGF Catalogus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AGF Catalogus
+//
+//  Created by Shaeme suister on 1/6/20.
+//  Copyright © 2020 Shaeme suister. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
